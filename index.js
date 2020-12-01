59124dffa15d3e7ec2eb2b8185b20365d8d371bb
sebastian8080 <sebas25211@hotmail.com>
2020-12-01 16:18:36 -0500

    Arreglando el evento para el touchstart

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ window.addEventListener("keydown", () => {
     window.location.href = 'sections.html';
 });
 
-windows.addEventListener("touchstart", () => {
+window.addEventListener("touchstart", () => {
     window.location.href = 'sections.html';
 });
